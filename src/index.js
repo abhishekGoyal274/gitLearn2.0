05fab23671081d2ffe51c3a334cf035680f4b24d
Abhishek Goyal <Abhishekgoyal274@gmail.com>
2022-12-24 18:17:54 +0530

    revert used

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ console.log("Master after 2nd branch alalalal");
 console.log("Hello");
 console.log("Master after 2nd branch alalalal");
 console.log("Hello");
-console.log("Master after 2nd branch alalalal");
+console.log("Master after 2nd branch");
